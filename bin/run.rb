89da6ef57d4594920d63517be90a88102ddcdcf4
github username <github email address>
2018-03-29 14:29:01 -0400

    adding query for seed data

--- bin/run.rb
+++ bin/run.rb
@@ -3,3 +3,18 @@ require_relative 'config/environment'
 def greeting
 	puts "Hello! Welcome to Flatiron BnB!"
 end
+
+def query 
+	puts "Enter a city (DC, LA, or NYC):"
+	loc = gets.chomp
+	if loc == "DC" || loc == "dc"
+		seedDC(dcpath)
+	elsif loc == "LA" || loc == "la"
+		seedLANYC(lapath)
+	elsif loc == "NYC" || loc == "nyc"
+		seedLANYC(nycpath)
+	else
+		query
+	end
+end
+
