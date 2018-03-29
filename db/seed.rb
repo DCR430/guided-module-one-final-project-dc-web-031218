374d4092c409bcfd9c2ddc174cb766865604bbab
SaturdayAM <jwl62@georgetown.edu>
2018-03-29 14:12:55 -0400

    LA and NYC data parsed and added to db

--- db/seed.rb
+++ db/seed.rb
@@ -1,12 +1,12 @@
 #Paths
 dcpath = 'db/abnb_data/DC_data.json'
+lapath = 'db/abnb_data/LA_data.json'
+nycpath = 'db/abnb_data/NYC_data.json'
 
 #Seeds DC Data
 def seed_DC(filepath)
 	file = File.read(filepath)
 	data_hash = JSON.parse(file)["taskCollectionResults"][0]["results"]
-
-
 	#vars for cols
 	data_hash.each do |hash|
 		id = hash["object"]["listing"][0]["id"]
@@ -36,9 +36,47 @@ def seed_DC(filepath)
 		}
 		Listing.create(temp_hash)
 	end
-
-	# rate = data_hash[0]["object"]["pricing_quote"][0]["rate"][0]["amount"]
-	puts "Arbitrary"
 end
 
-seed_DC(dcpath)
+def seed_LANYC(filepath)
+
+	file = File.read(filepath)
+	data_hash = JSON.parse(file)
+	listing_arr = data_hash["explore_tabs"][0]["sections"][0]["listings"]
+
+	listing_arr.each do |hash|
+		id = hash["listing"]["id"]
+		name = hash["listing"]["name"]
+		lat = hash["listing"]["lat"]
+		lng = hash["listing"]["lng"]  #
+		city = hash["listing"]["city"] #
+		neighborhood = hash["listing"]["neighborhood"] #
+		rate = hash["pricing_quote"]["rate"]["amount"] #
+		room_type = hash["listing"]["room_type"]
+		bedrooms = hash["listing"]["bedrooms"]
+		beds = hash["listing"]["beds"]
+		bathrooms = hash["listing"]["bathrooms"]
+
+		temp_hash = {
+			:id => id, 
+			:name => name,
+			:lat => lat,
+			:lng => lng,
+			:city => city,
+			:neighborhood => neighborhood,
+			:rate => rate,
+			:room_type => room_type,
+			:bedrooms => bedrooms,
+			:beds => beds,
+			:bathrooms => bathrooms
+		}
+
+		Listing.create(temp_hash)
+	end
+	
+end
+
+
+# seed_DC(dcpath)
+seed_LANYC(lapath)
+seed_LANYC(nycpath)
