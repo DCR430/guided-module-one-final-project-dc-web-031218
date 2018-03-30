b38b63b43dc7aee10a90a797998f2da1213c5d1e
SaturdayAM <jwl62@georgetown.edu>
2018-03-30 15:28:45 -0400

    Final commits

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,6 @@ require 'readline'
 Bundler.require
 
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
-ßActiveRecord::Base.logger = nil
+ActiveRecord::Base.logger = nil
 require_all 'lib'
 require_all 'app'
